09ab196162b776c1464625985cae6210602c091e
wanghao75 <86595007+wanghao75@users.noreply.github.com>
2022-09-08 20:41:43 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -193,9 +193,9 @@ LOGOUT_ON_CONFIRMATION = True
 
 # The two hosts are here so the main site can be over plain HTTP
 # while the voting URLs are served over SSL.
-URL_HOST = get_from_env("URL_HOST", "http://localhost:8000").rstrip("/")
+#URL_HOST = get_from_env("URL_HOST", "http://localhost:8000").rstrip("/")
 #URL_HOST = get_from_env("URL_HOST", "http://119.13.86.227:8000").rstrip("/")
-#URL_HOST = get_from_env("URL_HOST", "http://0.0.0.0:8000").rstrip("/")
+URL_HOST = get_from_env("URL_HOST", "http://0.0.0.0:8000").rstrip("/")
 #URL_HOST = get_from_env("URL_HOST", "https://helios-voting.test.osinfra.cn").rstrip("/")
 
 # IMPORTANT: you should not change this setting once you've created
