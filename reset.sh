8be47375d0d72e173096953564c3f0e5591be650
wanghao75 <86595007+wanghao75@users.noreply.github.com>
2022-09-09 14:31:53 +0800

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -5,4 +5,5 @@ dropdb helios
 createdb helios
 python manage.py makemigrations
 python manage.py migrate
+python manage.py migrate helios
 echo "from helios_auth.models import User; User.objects.create(user_type='github',user_id='wanghao75', info={'name':'Hao Wang'})" | python manage.py shell
