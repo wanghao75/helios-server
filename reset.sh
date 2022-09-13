d956d31c03a2e7cec78c9a14af8f8c0b90fcb52a
wanghao75 <86595007+wanghao75@users.noreply.github.com>
2022-09-13 16:58:40 +0800

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -4,4 +4,5 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 createdb helios
 python manage.py makemigrations
 python manage.py migrate
+python manage.py migrate helios
 echo "from helios_auth.models import User; User.objects.create(user_type='github',user_id='wanghao75', info={'name':'Hao Wang'})" | python manage.py shell
