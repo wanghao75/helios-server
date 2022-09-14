ad58622897f71d664952c2a6fe75427b3bd677b3
wanghao75 <shalldows@163.com>
2022-09-14 14:52:13 +0800

    fix

--- settings.py
+++ settings.py
@@ -58,8 +58,8 @@ if get_from_env('DATABASE_URL', None):
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-#TIME_ZONE = 'America/Los_Angeles'
-TIME_ZONE = 'Asia/ShangHai'
+TIME_ZONE = 'America/Los_Angeles'
+#TIME_ZONE = 'Asia/ShangHai'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -71,7 +71,7 @@ SITE_ID = 1
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
-USE_TZ = False
+#USE_TZ = False
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
