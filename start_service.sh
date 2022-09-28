848a94b85d2de36dc1568a8b4460fcda43c643d5
wanghao75 <86595007+wanghao75@users.noreply.github.com>
2022-09-28 14:26:38 +0800

    Update start_service.sh

--- start_service.sh
+++ start_service.sh
@@ -17,7 +17,7 @@ python3.6 -m pip install uwsgi --quiet
 #expect eof
 #EOF
 
-chmod 755 reset.sh
-sh /app/helios-server/reset.sh
+#chmod 755 reset.sh
+#sh /app/helios-server/reset.sh
 nohup celery -A helios worker -l INFO>celery.log &
 uwsgi --ini uwsgi.ini
